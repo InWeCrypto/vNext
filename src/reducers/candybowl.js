16fed97c25baf9c6bce55852cd70c6f209431c6d
杨叙 <yangxu@yangxudeMacBook-Pro.local>
2017-12-14 18:40:44 +0800

    222

--- src/reducers/candybowl.js
+++ src/reducers/candybowl.js
@@ -1,12 +1,15 @@
-import { CANDYBOWLBYMONTH } from "../actionTypes/";
+import { CANDYBOWLBYMONTH, MUSTREAD } from "../actionTypes/";
 const initState = {
-	candyBowlByMonth: null
+	candyBowlByMonth: null,
+	mustRead: null
 };
 
 const candayBowlData = (state = initState, action) => {
 	switch (action.type) {
 		case CANDYBOWLBYMONTH:
 			return { ...state, candyBowlByMonth: action.data };
+		case MUSTREAD:
+			return { ...state, mustRead: action.data };
 		default:
 			return state;
 	}
