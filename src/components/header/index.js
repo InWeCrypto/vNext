346856b1692cebc4b16d9d6cb01cb70ac7507faf
杨叙 <yangxu@yangxudeMacBook-Pro.local>
2018-01-09 15:40:39 +0800

    11

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,60 @@
 import React, { PureComponent } from "react";
 import "./index.less";
+import defaultHeader from "../../assets/images/member_img.png";
 class Header extends PureComponent {
+	constructor() {
+		super();
+		this.state = {
+			showMember: false
+		};
+		document.addEventListener("click", () => {
+			this.setState({
+				showMember: false
+			});
+		});
+	}
 	componentDidMount() {}
+	toggleMember(e) {
+		e.stopPropagation();
+		this.setState({
+			showMember: !this.state.showMember
+		});
+	}
 	render() {
+		const { showMember } = this.state;
 		return (
 			<div className="header-box ui start">
 				<div className="heder-left">1</div>
 				<div className="heder-middle f1">InWeCrypto</div>
 				<div className="heder-right">
-					<div className="member">
+					<div
+						className="member"
+						onClick={e => {
+							this.toggleMember(e);
+						}}
+					>
 						<i className="member-info" />
 					</div>
-					<div className="member-more">
-						<div className="member-item">
-							<i>22</i>
-							<span className="member-itemtext">222</span>
+					{showMember && (
+						<div className="member-more">
+							<div className="member-item">
+								<i>22</i>
+								<span className="member-itemtext">
+									未读消息
+								</span>
+							</div>
+							<div className="member-item">
+								<i>22</i>
+								<span className="member-itemtext">
+									个人中心
+								</span>
+							</div>
+							<div className="member-item">
+								<i>22</i>
+								<span className="member-itemtext">退出</span>
+							</div>
 						</div>
-					</div>
+					)}
 				</div>
 			</div>
 		);
