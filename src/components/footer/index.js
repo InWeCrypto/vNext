1099d5c3ae4d49bb93752afc99e6221de6677a23
杨叙 <yangxu@yangxudeMacBook-Pro.local>
2018-01-09 18:07:26 +0800

    11

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -8,9 +8,21 @@ class Footer extends PureComponent {
 				<div className="container ui center">
 					<div className="left">
 						<span className="sp">@InWeCrypto 2017</span>
+						<span className="item">
+							<i className="icon-tele" />
+						</span>
+						<span className="item">
+							<i className="icon-wechat" />
+						</span>
+						<span className="item">
+							<i className="icon-mail" />
+						</span>
 					</div>
-					<div className="f1">22323</div>
-					<div className="right">sss</div>
+					<div className="f1" />
+					<div className="right">
+						<div className="language-type">EN</div>
+						<div className="language-type cur">ZH</div>
+					</div>
 				</div>
 			</div>
 		);
---
+++ src/components/menu/index.js
@@ -0,0 +1,8 @@
+import React, { PureComponent } from "react";
+
+class MenuBox extends PureComponent {
+	render() {
+		return <div>22</div>;
+	}
+}
+export default MenuBox;
