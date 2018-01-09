d5cc2b51e2bcaebf43a9f6123a548114bf66eddc
munachar <hdh_angular@163.com>
2018-01-09 18:23:07 +0800

    indes

--- src/entries/project/components/root/index.js
+++ src/entries/project/components/root/index.js
@@ -4,32 +4,38 @@ import Footer from "../../../../components/footer";
 import "./index.less";
 
 export default class Root extends PureComponent {
-	componentDidMount() {
-		this.props.getProjectList();
-	}
-	render() {
-		return (
-			<div className="container">
-				<Header />
-				<div className='project ui'>
-					<div style={{width:582,color:'red'}}></div>
-					<div className='projectContent f1'>
-						<ul className='ui'>
-							<li>
-								<div className='projectLiTop ui'></div>	
-							</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-						</ul>
-					</div>
-				</div>
-				<Footer />
-			</div>
-		);
-	}
+    componentDidMount() {
+        this.props.getProjectList();
+    }
+    render() {
+        return (
+            <div className="container">
+                <Header />
+                <div className='project ui'>
+                    <div style={{ width: 582, color: 'red' }}></div>
+                    <div className='projectContent f1'>
+                        <ul className='ui'>
+                            <li>
+                                <div className='projectLiTop ui center'>
+                                    <div className='projectLiTopleft ui center'>
+                                        <img src='' />
+                                        <span>NEO</span>
+                                        <b>(neo)</b>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>1</li>
+                            <li>1</li>
+                            <li>1</li>
+                            <li>1</li>
+                            <li>1</li>
+                            <li>1</li>
+                            <li>1</li>
+                        </ul>
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        );
+    }
 }
