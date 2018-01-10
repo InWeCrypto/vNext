025ee1f6b2a652b35dd3617f44f0f94f7bf9f2cf
munachar <hdh_angular@163.com>
2018-01-10 21:28:27 +0800

    projectList

--- src/entries/projectlist/components/root/index.js
+++ src/entries/projectlist/components/root/index.js
@@ -58,38 +58,6 @@ export default class Root extends PureComponent {
 												</div>
 												<div
 													className={
-														index == 4
-															? "projectListLiTopRight collect"
-															: "projectListLiTopRight nocollect"
-													}
-												/>
-											</div>
-											<div className="projectListLiCenter">
-												<div className="left">
-													Blockchain
-												</div>
-												<div className="right">
-													$90.00<span>(-12.00%)</span>
-												</div>
-											</div>
-										</li>
-										<li>
-											<div className="projectListLiTop ui center">
-												<div className="projectListLiTopLeft ui center">
-													<div className="projectListImg newMsg">
-														<img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1515589318224&di=6418f077b77d7451a1246c6cfe793406&imgtype=0&src=http%3A%2F%2Fpic36.nipic.com%2F20131124%2F6608733_084856944000_2.jpg" />
-													</div>
-													<p>
-														<span className="ellitext">
-															NEO
-														</span>
-														<b className="ellitext">
-															(neo)
-														</b>
-													</p>
-												</div>
-												<div
-													className={
 														index == 2
 															? "projectListLiTopRight collect"
 															: "projectListLiTopRight nocollect"
@@ -100,9 +68,13 @@ export default class Root extends PureComponent {
 												<div className="left">
 													Blockchain
 												</div>
-												<div className="right">
-													$90.00<span>(-12.00%)</span>
-												</div>
+												{index == 0 && (
+													<div className="right">
+														$90.00<span>
+															(-12.00%)
+														</span>
+													</div>
+												)}
 											</div>
 										</li>
 									</ul>
