c3c627815bda338333dacd4fb1dd862726249397
munachar <hdh_angular@163.com>
2018-01-12 10:13:12 +0800

    routes

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,9 @@ import Project from "./entries/project";
 import ProjectList from "./entries/projectlist";
 import ProjectOpen from "./entries/projectopen";
 import News from "./entries/news";
-<<<<<<< HEAD
 import NewsDetail from "./entries/newsdetail";
-
-=======
 import Member from "./entries/member";
->>>>>>> a09506a10de971da47e7a0e2693598da05d1bfd1
+
 export default () => {
 	return (
 		<Switch>
@@ -19,11 +16,8 @@ export default () => {
 			<Route path="/projectlist" component={ProjectList} />
 			<Route path="/projectopen" component={ProjectOpen} />
 			<Route path="/news" component={News} />
-<<<<<<< HEAD
 			<Route path="/newsdetail" component={NewsDetail} />
-=======
 			<Route path="/member" component={Member} />
->>>>>>> a09506a10de971da47e7a0e2693598da05d1bfd1
 		</Switch>
 	);
 };
