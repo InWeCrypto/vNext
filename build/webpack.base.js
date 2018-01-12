9ba4b4a64a5072f70b5d6169e763021752907340
杨叙 <yangxu@yangxudeMacBook-Pro.local>
2018-01-12 18:31:45 +0800

    11

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -46,9 +46,6 @@ var webpackConfig = {
 				include: /node_modules/,
 				loader: require.resolve("babel-loader"),
 				options: {
-					// This is a feature of `babel-loader` for webpack (not Babel itself).
-					// It enables caching results in ./node_modules/.cache/babel-loader/
-					// directory for faster rebuilds.
 					cacheDirectory: true,
 					plugins: [
 						["react-html-attrs"],
