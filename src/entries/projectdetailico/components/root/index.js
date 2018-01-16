7db566e45d9298aae2a3f6e885121442dc31c29a
munachar <hdh_angular@163.com>
2018-01-16 22:06:32 +0800

    Ico

--- src/entries/projectdetailico/components/root/index.js
+++ src/entries/projectdetailico/components/root/index.js
@@ -12,104 +12,20 @@ import { platform } from "os";
 export default class Root extends PureComponent {
 	componentWillReceiveProps(nextProps) {}
 	componentDidMount() {
-		document.title = "InWe-News";
+		document.title = "InWe-ICO";
 		this.props.getProjectDetailIco();
 		let minH = getMainMinHeight();
-		let leftArrow = document.getElementById("newsDetailLeft").offsetLeft;
-		document.getElementById("newsDetailRight").style.right =
-			leftArrow + "px";
 		this.setState({
 			minH: minH
 		});
 		document.querySelector("#mainBox").style.minHeight = minH + "px";
 	}
-	componentDidUpdate() {
-		this.textChange();
-		this.creatVideo();
-	}
+	componentDidUpdate() {}
 	constructor(props) {
 		super(props);
 		this.state = {
-			minH: "auto",
-			isShowImg: true,
-			newsType: "video", // video img
-			isJump: "" // yes no
+			minH: "auto"
 		};
-	}
-	inFocus() {
-		this.setState({
-			isShowImg: false
-		});
-	}
-	outFocus() {
-		this.setState({
-			isShowImg: true
-		});
-	}
-	textChange() {
-		let obj = document.getElementById("textareaId");
-		if (obj.value.length > 300) obj.value = obj.value.slice(0, 300);
-		if (true) {
-			obj.style.height = 0 + "px";
-			obj.style.height =
-				obj.clientHeight + obj.scrollHeight - obj.clientHeight + "px";
-		}
-	}
-	textEmpty() {
-		let obj = document.getElementById("textareaId");
-		obj.value = "";
-		this.textChange();
-	}
-	textSubmit() {
-		let obj = document.getElementById("textareaId");
-		Msg.alert(obj.value);
-	}
-	videoPlay() {
-		this.setState({
-			newsType: ""
-		});
-		if (this.state.isJump == "yes") {
-			// 跳转
-			window.open("www.baidu.com");
-		} else {
-			// 不跳转
-			this.setState(
-				{
-					isJump: "no"
-				},
-				this.creatVideo()
-			);
-		}
-	}
-	creatVideo() {
-		let script = document.createElement("script");
-		script.src = "//g.alicdn.com/de/prismplayer/2.5.0/aliplayer-min.js";
-		script.type = "text/javascript";
-		let link = document.createElement("link");
-		link.href =
-			"//g.alicdn.com/de/prismplayer/2.5.0/skins/default/aliplayer-min.css";
-		link.rel = "stylesheet";
-		document.getElementsByTagName("head")[0].appendChild(link);
-		document.getElementsByTagName("head")[0].appendChild(script);
-		// window.onload = function() {
-		if (this.state.isJump == "no") {
-			var player = new Aliplayer(
-				{
-					id: "J_prismPlayer",
-					width: "100%",
-					autoplay: false,
-					cover:
-						"https://b-ssl.duitang.com/uploads/item/201801/10/20180110212314_ytxcG.thumb.700_0.jpeg",
-					//支持播放地址播放,此播放优先级最高
-					source: "http://abc.tanshikeji.com/video1.mp4"
-				},
-				function(player) {
-					console.log("播放器创建好了。");
-					console.log(player.play());
-				}
-			);
-		}
-		// };
 	}
 	render() {
 		const { minH } = this.state;
@@ -120,191 +36,110 @@ export default class Root extends PureComponent {
 					<div className="container">
 						<FixedMenu changeLng={changeLng} lng={lng} />
 						<Header />
-						<div id="mainBox" className="newsDetail ui">
-							<div
-								id="newsDetailLeft"
-								className="newsDetailLeft ui center"
-							>
-								<Link
-									to={{
-										pathname: "/project",
-										search: ""
-									}}
-								>
-									<span />
-								</Link>
-							</div>
-							<div className="newsDetailCon">
-								<div className="newsDetailConTitle">
-									<span>
-										纽约州议员提出四项区块链技术相关法案
-									</span>
-								</div>
-								<div className="newsMeta">
-									<div className="newsDetailConMeta">
-										<span className="metaDate">
-											2017-11-16 11:35:33
-										</span>
-										<span className="metaCategory">
-											原创
-										</span>
-										<img
-											src="http://down1.sucaitianxia.com/psd02/psd169/psds32262.jpg"
-											alt=""
-										/>
-										<img
-											src="http://down1.sucaitianxia.com/psd02/psd169/psds32262.jpg"
-											alt=""
-										/>
-										<img
-											src="http://down1.sucaitianxia.com/psd02/psd169/psds32262.jpg"
-											alt=""
-										/>
-									</div>
-									<div className="newDetailConShare">
-										<span>
-											<b className="wxShare" />
-										</span>
-										<span>
-											<b className="collect active" />
-										</span>
-									</div>
-								</div>
-								<div className="newsDetailBox">
-									{/* 视频 */}
-									{this.state.isJump == "no" && (
-										<div
-											className="prism-player"
-											id="J_prismPlayer"
-										/>
-									)}
-									{this.state.newsType == "video" && (
-										<div className="videoType">
-											<img
-												src="https://b-ssl.duitang.com/uploads/item/201801/10/20180110212314_ytxcG.thumb.700_0.jpeg"
-												alt=""
-											/>
-											<b
-												id="videoPlay"
-												onClick={() => {
-													this.videoPlay();
-												}}
-											/>
-										</div>
-									)}
-
-									<div className="newsDetailContent">
-										<p>
-											NEO是一个非盈利的社区化的区块链项目，是利用区块链技术和数字身份进行资
-											产数字化，利用智能合约对数字资产进行自动化管理，实现“智能经济”的一种分
-											布式网络。NEO于2014年正式立项，2015年6月在Github上实时开源，自成立以
-											来，NEO团队亲历了区块链行业的高潮与低谷，数字货币市场的狂热与冷却。我
-											们相信，科技是这个时代变迁的原动力，在这股动力的推动下，我们将迈入新
-											的“智能经济”时代。
-										</p>
-										{this.state.newsType == "img" && (
-											<img
-												src="http://img4.imgtn.bdimg.com/it/u=4004954884,1272926999&fm=214&gp=0.jpg"
-												alt=""
-											/>
-										)}
-
-										<p>
-											NEO是一个非盈利的社区化的区块链项目，是利用区块链技术和数字身份进行资
-											产数字化，利用智能合约对数字资产进行自动化管理，实现“智能经济”的一种分
-											布式网络。NEO于2014年正式立项，2015年6月在Github上实时开源，自成立以
-											来，NEO团队亲历了区块链行业的高潮与低谷，数字货币市场的狂热与冷却。我
-											们相信，科技是这个时代变迁的原动力，在这股动力的推动下，我们将迈入新
-											的“智能经济”时代。
-										</p>
-									</div>
-									<p className="newsReadNums">13人已读</p>
-								</div>
-								<div className="newsDetailComment">
-									<div className="newsDetailCommentNums">
-										<b>1</b>条评论
-									</div>
-									<div className="newsDetailCommmentBox">
-										<div className="newsDetailCommentBoxCenter ui center">
-											{this.state.isShowImg && (
-												<div className="newsDetailHeadImg">
-													<img
-														src="http://a.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=bbba1da0d60735fa91a546bdab612385/9825bc315c6034a84e7d073ac9134954082376e9.jpg"
-														alt=""
-													/>
-												</div>
-											)}
-											<textarea
-												name=""
-												id="textareaId"
-												placeholder="说点什么..."
-												onFocus={() => {
-													this.inFocus();
-												}}
-												onBlur={() => {
-													this.outFocus();
-												}}
-												onChange={() => {
-													this.textChange();
-												}}
-											/>
-										</div>
-										<div className="newsDetailCommentBoxBtn clearfix">
-											<span
-												className="submit"
-												onClick={() => {
-													this.textSubmit();
-												}}
-											>
-												提交
-											</span>
-											<span
-												className="cancel"
-												onClick={() => {
-													this.textEmpty();
-												}}
-											>
-												取消
-											</span>
-										</div>
-									</div>
-									<ul className="newsDetailCommentList">
-										<li>
-											<div className="newsDetailCommentListHead ui center">
-												<div className="newsDetailHeadImg">
-													<img
-														src="http://a.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=bbba1da0d60735fa91a546bdab612385/9825bc315c6034a84e7d073ac9134954082376e9.jpg"
-														alt=""
-													/>
-												</div>
-												<div className="newsDetailHeadInfo">
-													<span className="newsDetailHeadName">
-														想要一只猫
-													</span>
-													<span className="newsDetailHeadDate">
-														2017.11.11 12:30:11
-													</span>
-												</div>
+						<div id="mainBox" className="projectDetail ui">
+							<div className="projectDetailCon1">
+								<div className="projectDetailConTop ui">
+									<div className="projectDetailConTopLeft">
+										<div className="projectDetailCenter1">
+											<div className="projectDetailImg">
+												<img src="https://timgsa.baidu.com/timg?image&amp;quality=80&amp;size=b9999_10000&amp;sec=1515589318224&amp;di=6418f077b77d7451a1246c6cfe793406&amp;imgtype=0&amp;src=http%3A%2F%2Fpic36.nipic.com%2F20131124%2F6608733_084856944000_2.jpg" />
 											</div>
-											<div className="newsDetailCommentListContent">
-												<p>很有作用</p>
-											</div>
-										</li>
-									</ul>
+											<span>NEO</span>
+											<p>Blockchain</p>
+										</div>
+
+										<div className="projectDetailCenter2">
+											<span>Active</span>
+											<p>
+												Token Sale<i>ends in 4 DAYS</i>
+											</p>
+										</div>
+									</div>
+									<div className="projectDetailConTopRight">
+										分享
+									</div>
+								</div>
+								<div className="projectDetailCon1Box ui">
+									<div className="projectDetailCon1BoxLeft">
+										<div className="projectDetailCon1BoxLeftDate">
+											<b />
+											<p>12.22-01.02</p>
+										</div>
+										<ul>
+											<li>Ticker: LOCICOIN</li>
+											<li>Token type: ERC20 ICO</li>
+											<li>
+												Token Price: 1 LOCICOIN = 0.44
+												USD
+											</li>
+											<li>
+												Fundraising Goal: 19,000,000 USD
+											</li>
+											<li>
+												Sold on pre-sale: 1,661,884 USD
+											</li>
+											<li>Total Tokens: 100,000,000</li>
+											<li>
+												Available for Token Sale: 50%
+											</li>
+											<li>
+												Bonus for the First: 25%
+												DISCOUNT
+											</li>
+											<li>
+												Min/Max Personal Cap: 0.1 ETH /
+												TBA
+											</li>
+											<li>Accepts: ETH</li>
+										</ul>
+									</div>
+									<div className="projectDetailCon1BoxRight">
+										<div className="projectDetailCon1BoxRightTit">
+											ICO 结构
+										</div>
+										<ul>
+											<li>
+												<b />
+												<span>25%用于团队建设</span>
+											</li>
+										</ul>
+									</div>
 								</div>
 							</div>
-							<div
-								id="newsDetailRight"
-								className="newsDetailRight show ui center"
-							>
-								<Link
-									to={{
-										pathname: "/project",
-										search: ""
-									}}
-								>
-									<span />
-								</Link>
+							<div className="projectDetailCon2">
+								<div className="projectDetailCon2Box">
+									<div className="projectDetailCon2Title">
+										Rank
+									</div>
+									<p>+关注热度：第1名</p>
+									<p>+用户评分：4.5</p>
+								</div>
+								<div className="projectDetailCon2Box">
+									<div className="projectDetailCon2Title">
+										Explore
+									</div>
+									<p>+Neotracker.io</p>
+									<p>+Neotracker.io Copy</p>
+								</div>
+								<div className="projectDetailCon2Box">
+									<div className="projectDetailCon2Title">
+										Wallet
+									</div>
+									<p>+Im token</p>
+								</div>
+							</div>
+							<div className="projectDetailCon3">
+								<ul className="projectDetailCon3Ul">
+									<li>
+										<span>项目概况</span>
+									</li>
+									<li>
+										<span>项目资讯</span>
+									</li>
+									<li>
+										<span>项目介绍</span>
+									</li>
+								</ul>
 							</div>
 						</div>
 					</div>
