37214f9ff045f1a2861eee7d10bf59c5669542f3
munachar <hdh_angular@163.com>
2018-01-17 16:10:16 +0800

    asd

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import Home from "./entries/home";
 import Project from "./entries/project";
 import ProjectList from "./entries/projectlist";
 import ProjectOpen from "./entries/projectopen";
-import ProjectDetailIco from "./entries/projectdetailico";
+import ProjectDetail from "./entries/projectdetail";
 import News from "./entries/news";
 import NewsDetail from "./entries/newsdetail";
 import Trading from "./entries/trading";
@@ -19,7 +19,7 @@ export default () => {
 			<Route path="/project" component={Project} />
 			<Route path="/projectlist" component={ProjectList} />
 			<Route path="/projectopen" component={ProjectOpen} />
-			<Route path="/projectdetailico" component={ProjectDetailIco} />
+			<Route path="/projectdetail" component={ProjectDetail} />
 			<Route path="/news" component={News} />
 			<Route path="/newsdetail" component={NewsDetail} />
 			<Route path="/trading" component={Trading} />
