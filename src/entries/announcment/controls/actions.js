172ce79ea1332594900d7d46f6755f4306cd7e82
曹丹阳 <caodanyang@ACA815C6.ipt.aol.com>
2018-01-27 13:44:46 +0800

    123

--- src/entries/announcment/controls/actions.js
+++ src/entries/announcment/controls/actions.js
@@ -3,8 +3,16 @@ import http from "../../../utils/ajax";
 
 const PRE_FIX = "ANNOUNCMENT_";
 export const ANNOUNCMENT = `${PRE_FIX}ANNOUNCMENT`;
+export const ANNOUNCMENTM = `${PRE_FIX}ANNOUNCMENTM`;
 
 export const getAnnouncment = createAction(ANNOUNCMENT, params => {
+	return http.get({
+		url: "exchange_notice",
+		params: params
+	});
+});
+
+export const getAnnouncmentM = createAction(ANNOUNCMENTM, params => {
 	return http.get({
 		url: "exchange_notice",
 		params: params
